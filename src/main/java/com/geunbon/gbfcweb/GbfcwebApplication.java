7d2257b3f683a5ecf50a20207c2cfc72550a504b
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-09-17 21:56:07 +0900

    chore : 초기 세팅

---
+++ src/main/java/com/geunbon/gbfcweb/GbfcwebApplication.java
@@ -0,0 +1,13 @@
+package com.geunbon.gbfcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbfcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbfcwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geunbon/gbfcweb/GbfcwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geunbon.gbfcweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbfcwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
